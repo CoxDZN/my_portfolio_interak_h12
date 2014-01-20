134461a857a7f696ce53345506774d889635cc8f
Christopher Cox <coxdzn@outlook.com>
2014-01-20 10:33:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoxdznPortfolio::Application.config.secret_key_base = 'e22a96a5489d32963a6f20b98da5e43065567dc3bd8f6cabf647c95c8d4cffd9b328b18a01685996f8e5df10f28f424ea6a9587ea7c2047608b451770af06fcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoxdznPortfolio::Application.config.session_store :cookie_store, key: '_coxdzn_portfolio_session'
